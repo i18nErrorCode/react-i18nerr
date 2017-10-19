cf2b53bea39144164a12d98783e96f0ba3547f39
lany9527 <lany9527@gmail.com>
2017-10-19 16:05:52 +0800

    refactoring header component

--- src/component/header.js
+++ src/component/header.js
@@ -1,11 +1,11 @@
 /**
  * Created by axetroy on 17-4-6.
  */
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Menu, Icon } from 'antd';
-import { NavLink, matchPath, withRouter } from 'react-router-dom';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Menu, Icon, Row, Col } from "antd";
+import { NavLink, matchPath, withRouter } from "react-router-dom";
+import StatusBar from '../component/statusBar';
 class Header extends Component {
   static propTypes = {
     match: PropTypes.object.isRequired,
@@ -16,64 +16,62 @@ class Header extends Component {
     super(props);
     this.state = {
       nav: [
-        { path: '/', name: 'home', title: 'Home', icon: <Icon type="home" /> },
-        {
-          path: '/login',
-          title: '登录',
-          icon: ''
-        },
-        {
-          path: '/register',
-          title: '注册',
-          icon: ''
-        }
+        { path: "/", name: "home", title: "Home", icon: <Icon type="home" /> },
+        { path: "/table", name: "table", title: "我的集合", icon: <Icon type="hdd" /> }
       ].filter(v => v)
     };
   }
   componentDidMount() {}
-
   render() {
     const pathname = this.props.location.pathname;
-    const navClassName = 'ant-menu-item-selected';
+    const navClassName = "ant-menu-item-selected";
     return (
       <div
         id="header"
         style={{
-          position: 'relative'
+          position: "relative"
         }}
       >
-        <Menu mode="horizontal">
-          {this.state.nav.map(nav => {
-            return (
-              <Menu.Item
-                key={nav.path}
-                className={(() => {
-                  const navPath = nav.path;
-                  const isMatchRoute = matchPath(pathname, {
-                    path: navPath
-                  });
-                  if (pathname === '/') {
-                    return pathname === navPath ? navClassName : '';
-                  } else {
-                    return isMatchRoute && navPath !== '/' ? navClassName : '';
-                  }
-                })()}
-              >
-                <NavLink
-                  to={nav.path}
-                  style={{
-                    fontSize: '1.4rem'
-                  }}
-                >
-                  {nav.icon ? nav.icon : ''}
-                  {nav.title}
-                </NavLink>
-              </Menu.Item>
-            );
-          })}
-        </Menu>
+        <Row>
+          <Col span={7} push={17}>
+            <StatusBar />
+          </Col>
+          <Col span={17} pull={7}>
+            <Menu mode="horizontal">
+              {this.state.nav.map(nav => {
+                return (
+                  <Menu.Item
+                    key={nav.path}
+                    className={(() => {
+                      const navPath = nav.path;
+                      const isMatchRoute = matchPath(pathname, {
+                        path: navPath
+                      });
+                      if (pathname === "/") {
+                        return pathname === navPath ? navClassName : "";
+                      } else {
+                        return isMatchRoute && navPath !== "/" ? navClassName : "";
+                      }
+                    })()}
+                  >
+                    <NavLink
+                      to={nav.path}
+                      style={{
+                        fontSize: "1.4rem"
+                      }}
+                    >
+                      {nav.icon ? nav.icon : ""}
+                      {nav.title}
+                    </NavLink>
+                  </Menu.Item>
+                );
+              })}
+            </Menu>
+          </Col>
+        </Row>
       </div>
     );
   }
 }
+
 export default withRouter(Header);
---
+++ src/component/statusBar.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Menu } from "antd";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { store } from "../redux/user";
+
+class StatusBar extends Component {
+  logout() {
+    console.info(`logout`);
+    localStorage.removeItem("token");
+    this.props.storeUserInfo("");
+  }
+  render() {
+    return (
+      <div>
+        {!this.props.USER.username ? (
+          <Menu mode="horizontal">
+            <Menu.Item>
+              <NavLink to="/login">登录</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+              <NavLink to="/register">注册</NavLink>
+            </Menu.Item>
+          </Menu>
+        ) : (
+          <div>
+            <ul className="ant-menu ant-menu-horizontal ant-menu-light ant-menu-root">
+              <li className="ant-menu-item">您好：{this.props.USER.username}</li>
+              <li className="ant-menu-item">
+                <NavLink to="/user">个人中心</NavLink>
+              </li>
+              <li className="ant-menu-item">
+                <a onClick={this.logout.bind(this)}>登出</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+function connection(component) {
+  return connect(
+    function mapStateToProps(state) {
+      return {
+        USER: state.USER
+      };
+    },
+    function mapDispatchToProps(dispatch) {
+      return bindActionCreators(
+        {
+          storeUserInfo: store
+        },
+        dispatch
+      );
+    }
+  )(component);
+}
+export default connection(StatusBar);
+// <a onClick={this.logout.bind(this)}>登出</a>
