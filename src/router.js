2ddc07341796090d69dd467d9d5d4e83a550e6a2
littlestone <littlestone>
2017-12-11 16:23:55 +0800

    fix: 补充路由拦截

--- src/router.js
+++ src/router.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route, Switch, Redirect } from 'dva/router';
 import dynamic from 'dva/dynamic';
+import isAuthenticated from './utils/isAuthenticated';
+/* 保护路由组件 */
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => isAuthenticated() === true ?
+      (<Component {...props} />) :
+      (<Redirect to={{pathname: "/login", state: { from: props.location }}} />) } />
+);
 
 function RouterConfig({ history, app }) {
-  const Home = dynamic({
-    app,
-    models: () => [import('./models/home'), import('./models/tables')],
-    component: () => import('./routes/Home/Home'),
-  });
-  const UserTable = dynamic({
-    app,
-    models: () => [import('./models/tables')],
-    component: () => import('./routes/UserTable/UserTable'),
-  });
-  const UserRows = dynamic({
-    app,
-    models: () => [import('./models/rows')],
-    component: () => import('./routes/UserRows/UserRows'),
-  });
+  /* 需要登录认证的路由 */
+  const routes = [
+    {
+      path: '/',
+      models: () => [import('./models/home'), import('./models/tables')],
+      component: () => import('./routes/Home/Home'),
+    },
+    {
+      path: "/user-table",
+      models: () => [import('./models/tables')],
+      component: () => import('./routes/UserTable/UserTable'),
+    },
+    {
+      path: "/table-rows",
+      models: () => [import('./models/rows')],
+      component: () => import('./routes/UserRows/UserRows'),
+    }
+  ];
   const Login = dynamic({
     app,
     models: () => [import('./models/user')],
@@ -31,9 +43,18 @@ function RouterConfig({ history, app }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/user-table" exact component={UserTable} />
-        <Route path="/table-rows" exact component={UserRows} />
+        {
+          routes.map(({ path, ...dynamics }, key) => (
+            <PrivateRoute
+              exact
+              key={key}
+              path={path}
+              component={dynamic({
+                app,
+                ...dynamics
+              })} />
+          ))
+        }
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
       </Switch>
---
+++ src/utils/isAuthenticated.js
@@ -0,0 +1,20 @@
+const jwtDecode = require('jwt-decode');
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+ 
+  if(token) {
+    const decodedJWT = jwtDecode(token);
+    // console.log('jwt info ', decodedJWT);
+    const timeNow = new Date().getTime();
+    
+    if(timeNow >= decodedJWT.exp*1000) {
+      return false;
+    }
+    
+    return true;
+  }
+  return false;
+}
+
+export default isAuthenticated
